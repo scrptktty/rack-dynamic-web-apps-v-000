bb2abb9e5aa1dbe193994cb6be47c8c17fdacec2
Kachel Wittig <kjwittig@gmail.com>
2018-07-02 14:53:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,11 @@ class Application
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
 
+    if num_1 == num_2 && num_2 == num_3
+      resp.write "You win!"
+    else 
+      resp.write "You lose"
+
     resp.finish
   end
 
